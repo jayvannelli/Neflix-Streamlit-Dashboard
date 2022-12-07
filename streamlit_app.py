b747993e162bc14c00e92c921a2a3e947b907cc7
jayvannelli <jaymvannelli@gmail.com>
2022-12-07 13:51:05 -0500

    Remove streamlit profile report.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pandas_profiling
-
-from streamlit_pandas_profiling import st_profile_report
 
 
 @st.experimental_memo
@@ -15,28 +12,7 @@ def fetch_and_clean_data():
 def main():
     raw_credits, raw_titles = fetch_and_clean_data()
 
-    d = {
-        "Titles": raw_titles,
-        "Credits": raw_credits,
-    }
-
-    with st.sidebar:
-        st.title("Credits & Titles Data")
-
-        with st.form("report_selection_form"):
-            report_selection = st.selectbox("Select DataFrame to generate profile report for:",
-                                            options=["Titles", "Credits"])
-
-            submit_button = st.form_submit_button("Load profile report")
-
-    if submit_button:
-        pr = gen_profile_report(d[report_selection], explorative=True)
-        st_profile_report(pr)
-
-
-@st.cache(allow_output_mutation=True)
-def gen_profile_report(df, *report_args, **report_kwargs):
-    return df.profile_report(*report_args, **report_kwargs)
+    st.write(raw_titles, raw_credits)
 
 
 if __name__ == "__main__":
